9c006343cec1a34132bbc16cd0fdf0db1696947a
Patrycja <patty.zaw@gmail.com>
2021-09-30 17:52:39 +0200

    print+kalkulator+zad1

--- materialy-z-zajec/plik1.py
+++ materialy-z-zajec/plik1.py
@@ -1,8 +1,19 @@
-Sposób nr 1:
-print("Hej!")
-print("Witam")
+# Sposób nr 1:
+# print("Hej!")
+# print("Witam")
+#
+# # Sposób nr 2 - Slash+n : nowa linia
+# print("Hej! \nWitam")
+# # Slash+ tabulator
+# print("Hej! \tWitam")
 
-# Sposób nr 2 - Slash+n : nowa linia
-print("Hej! \nWitam")
-# Slash+ tabulator
-print("Hej! \tWitam")
+# Kalkulator:
+# print("22"*2) #2222 - powtarza str tyle ile po gwiazdce
+# print(2+3)
+# print(3-2)
+# print(2*2)
+# print(10/2)
+
+# Zadanie 1:
+print(23*24*60*60)
+
