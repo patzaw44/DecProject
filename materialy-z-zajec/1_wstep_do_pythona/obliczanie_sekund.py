811fb17f7ce8f5512f93fb79aaf1689066e84041
Patrycja <patty.zaw@gmail.com>
2021-09-30 19:07:20 +0200

    1Przykładowe rozwiązanie+ zmienne, nowe nazwy

--- materialy-z-zajec/1_wstep_do_pythona/obliczanie_sekund.py
+++ materialy-z-zajec/1_wstep_do_pythona/obliczanie_sekund.py
@@ -1,8 +1,9 @@
 ilosc_dni=23
-godziny=24
-min=60
-sek=60
+godziny_w_dniu=24
+minuty_w_godzinie=60
+sekundy_w_minucie=60
 
 
-print("Odpowiedź to" ,ilosc_dni*godziny*min*sek, "sekund.")
+print("Odpowiedź to" ,ilosc_dni*godziny_w_dniu*minuty_w_godzinie*sekundy_w_minucie, "sekund.")
 print("Odpowiedź to" ,23*24*60*60, "sekund.")
+print(f"Odpowiedź to {ilosc_dni*godziny_w_dniu*minuty_w_godzinie*sekundy_w_minucie} sekund.")
