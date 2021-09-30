2277bf057efd0e155e3f5f468547fc67ea7ba9a6
Patrycja <patty.zaw@gmail.com>
2021-09-30 18:28:32 +0200

    Przykładowe rozwiązanie

---
+++ materialy-z-zajec/tekst_piosenki.py
@@ -0,0 +1,16 @@
+# Zadanie 2:
+# Sposób 1
+print("When I was young\nI didn't know too much\nI thought that I could rule the world\nThen I grew up\nI found out life was hard\nHarder than stone")
+
+# Sposób 2
+print('''When I was young
+I didn't know too much
+I thought that I could rule the world
+Then I grew up
+I found out life was hard
+Harder than stone''')
+
+# Sposób 3
+print("linijka1")
+print("linijka2")
+print("linijka3")
