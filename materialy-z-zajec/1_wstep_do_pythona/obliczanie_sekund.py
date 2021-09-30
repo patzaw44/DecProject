c84caabddc431b581b76f401643f4354657b2a10
Patrycja <patty.zaw@gmail.com>
2021-09-30 18:53:34 +0200

    1Przykładowe rozwiązanie+ zmienne

---
+++ materialy-z-zajec/1_wstep_do_pythona/obliczanie_sekund.py
@@ -0,0 +1,8 @@
+ilosc_dni=23
+godziny=24
+min=60
+sek=60
+
+
+print("Odpowiedź to" ,ilosc_dni*godziny*min*sek, "sekund.")
+print("Odpowiedź to" ,23*24*60*60, "sekund.")
---
+++ materialy-z-zajec/1_wstep_do_pythona/zmienne.py
@@ -0,0 +1,10 @@
+ocena=2
+
+print(ocena)
+print("ocena")
+
+# zmienna nie powinna zaczynać się od liczby, nie powinno być myślnika
+# nie robimy:
+sghdyfweg=1
+
+
