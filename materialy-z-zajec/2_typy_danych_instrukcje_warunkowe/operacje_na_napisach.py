b3dae80efacbd545c7c62de266535a68708c0fac
Patrycja <patty.zaw@gmail.com>
2021-10-04 18:42:34 +0200

    input+ zadanie

--- materialy-z-zajec/2_typy_danych_instrukcje_warunkowe/operacje_na_napisach.py
+++ materialy-z-zajec/2_typy_danych_instrukcje_warunkowe/operacje_na_napisach.py
@@ -1,3 +1,8 @@
-odpowiedz = input("Podaj swoje imie: ")
+#odpowiedz = input("Podaj swoje imie: ")
 
-print(f"Twoja odpowiedz to: {odpowiedz.title()}.")
+#print(f"Twoja odpowiedz to: {odpowiedz.title()}.")
+
+wiek = int(input("Podaj swoj wiek:"))
+nowy_wiek =2021-wiek
+
+print(f"Twoja data urodzenia to : {nowy_wiek}.")
