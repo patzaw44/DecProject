fa188a4b9b584c56b270319808037fa2716a703a
Patrycja <patty.zaw@gmail.com>
2021-10-04 18:29:43 +0200

    operacje na liczbach pz

---
+++ materialy-z-zajec/2_typy_danych_instrukcje_warunkowe/funkcja_bool.py
@@ -0,0 +1,11 @@
+print(True)
+print(False)
+#sprawdzamy czy cos jest wartoscia zerowa
+print(bool(0))
+print(bool(11))
+print(bool(0))
+
+print(bool("Mateusz"))
+print(bool("0"))
+print(bool(""))
+print(bool(" "))
---
+++ materialy-z-zajec/2_typy_danych_instrukcje_warunkowe/porownania.py
@@ -0,0 +1,22 @@
+print(1<3)
+print(1>3)
+
+print(1<=1)
+
+print(5==5)
+print(5!=5)
+
+wiek_mateusz=13
+
+wiek_wojtek = 10
+
+print(wiek_mateusz<wiek_wojtek)
+
+limit_wiekowy_min= 10
+limit_wiekowy_max=12
+
+print(limit_wiekowy_min <= wiek_mateusz >= limit_wiekowy_max)
+
+
+limit_wiekowy_min= 10
+limit_wiekowy_max=12
