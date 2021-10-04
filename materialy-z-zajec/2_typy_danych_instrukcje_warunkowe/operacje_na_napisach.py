2aa751842d43487aeb75ff2dc8af36bd8dd698e6
Patrycja <patty.zaw@gmail.com>
2021-10-04 19:33:38 +0200

    indeksowanie

--- materialy-z-zajec/2_typy_danych_instrukcje_warunkowe/operacje_na_napisach.py
+++ materialy-z-zajec/2_typy_danych_instrukcje_warunkowe/operacje_na_napisach.py
@@ -5,3 +5,9 @@ print(f"Dlugosc imienia to {len(odpowiedz)}")
 print(odpowiedz[-1])
 print(f"Dlugosc imienia to {len(odpowiedz)-1}")
 print(f"Dlugosc imienia to {odpowiedz[len(odpowiedz)-1]}")
+#zakresy:
+print(f"{odpowiedz[:4]}")
+print(f"{odpowiedz[0:4]}")
+print(f"{odpowiedz[:]}")
+
+print(f"{odpowiedz[::2]}")# pobierz co drugą literę
