5f9e8e559fbc58006f29d98a7bffa54d5193133e
Patrycja <patty.zaw@gmail.com>
2021-10-04 19:25:32 +0200

    indeksowanie

---
+++ materialy-z-zajec/2_typy_danych_instrukcje_warunkowe/operacje_na_napisach.py
@@ -0,0 +1,7 @@
+odpowiedz=input("Podaj swoje imie: ")
+print(f"Twoja odpowiedz to: {odpowiedz.title()}.")
+
+print(f"Dlugosc imienia to {len(odpowiedz)}")
+print(odpowiedz[-1])
+print(f"Dlugosc imienia to {len(odpowiedz)-1}")
+print(f"Dlugosc imienia to {odpowiedz[len(odpowiedz)-1]}")
