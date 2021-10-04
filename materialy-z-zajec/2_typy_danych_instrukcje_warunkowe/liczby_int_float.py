2e6943a3aab2779e6103ca2c5d9c59debe8b0cf0
Patrycja <patty.zaw@gmail.com>
2021-10-04 17:26:25 +0200

    operacje na liczbach pz

---
+++ materialy-z-zajec/2_typy_danych_instrukcje_warunkowe/liczby_int_float.py
@@ -0,0 +1,17 @@
+#/
+print(10/3)
+
+#// : dzielenie bez reszty
+print(10//3)
+#operator modulo
+print(10%3)
+
+print(12+12*12)
+
+#abs()
+
+print(abs(-12))
+print(abs(12))
+
+import decimal
+print((decimal.Decimal)(decimal.Decimal))
