17b1db8fa2ac9b1defaeef7ca1997c1d7069f4e1
Patrycja <patty.zaw@gmail.com>
2021-10-07 19:26:55 +0200

    Zadanie if , elif, else

---
+++ materialy-z-zajec/3_napisy_instrukcje_warunkowe_petle/zadanie_przedstawienie.py
@@ -0,0 +1,12 @@
+"""Zadanie
+
+Napisz program, który pyta użytkownika o wiek oraz stan konta i sprawdza, czy może on wejść na przedstawienie.
+Zakładamy, że bilet na przedstawienie kosztuje 50 zł i jest ono dostępne od 16 lat."""
+
+wiek= int(input("Podaj wiek: "))
+stan_konta= float(input("Podaj stan konta: "))
+
+if wiek >= 16 and stan_konta > 50:
+    print("Użytkownik może wejść na przedstawienie.")
+else:
+    print("Użytkownik nie może wejść na przedstawienie.")
---
+++ materialy-z-zajec/Zadania domowe/homework_4_pz.py
@@ -0,0 +1,11 @@
+"""Zadanie
+
+Napisz program, który:
+
+    przypisze do zmiennej cenę za litr paliwa
+
+    przypisze do zmiennej średnie spalanie na 100 km
+
+    następnie zapyta użytkownika o cenę, którą jest w stanie zapłacić oraz długość trasy w km, jaką chce pokonać
+
+Program powinien wypisać komunikat z decyzją czy uda się przejechać podaną trasę po zatankowaniu auta za przytoczoną kwotę."""
