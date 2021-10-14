13f4992b9ce430a11e8aea53dc1602115ed7a928
Patrycja <patty.zaw@gmail.com>
2021-10-14 19:30:40 +0200

    laczenie list

---
+++ materialy-z-zajec/5_kolekcje/laczenie_list.py
@@ -0,0 +1,10 @@
+panstwa_europy = ["Polska", "Niemcy", "Słowacja"]
+panstwa_azja = ["Chiny", "Indie", "Japonia"]
+inne_panstwa = ["Nigeria", "Brazylia"]
+panstwa = panstwa_europy+panstwa_azja
+print(panstwa)
+print(panstwa_azja)
+print(panstwa_europy)
+
+
+
