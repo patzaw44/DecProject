170c6e4d8d79cbfa855bce77ac99827aff3aa6ff
Patrycja <patty.zaw@gmail.com>
2021-10-21 16:21:22 +0200

    zadanie10

--- materialy-z-zajec/Zadania domowe/homework_10_pz.py
+++ materialy-z-zajec/Zadania domowe/homework_10_pz.py
@@ -10,4 +10,24 @@ Wskazówka, przydatne mogą się okazać metody modułu random
 
 import random
 help(random.choice)
-help(random.shuffle)"""
+help(random.shuffle)
+"""
+
+import random
+
+slowa = ["książka", "zakładka", "płyta", "odtwarzacz", "lampka", "koc"]
+# losowanie wyrazu
+wyraz = random.choice(slowa)
+# print(wyraz)
+pomieszane_litery = list(wyraz)
+print(pomieszane_litery)
+# pomieszane litery w wyrazie
+random.shuffle(pomieszane_litery)
+print(pomieszane_litery)
+
+zagadka = input("Proszę o odgadnięcie słowa: ")
+
+if zagadka == wyraz:
+    print("Brawo! To jest prawidłowy wyraz!")
+else:
+    print("Niestety, to nie jest prawidłowy wyraz.")
