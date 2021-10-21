11eedcd6f86982d7e8e55d90fd75a907500f695c
Patrycja <patty.zaw@gmail.com>
2021-10-21 17:54:39 +0200

    funkcje z parametrami

--- materialy-z-zajec/7_funkcje/funkcje_z_parametrami.py
+++ materialy-z-zajec/7_funkcje/funkcje_z_parametrami.py
@@ -6,6 +6,13 @@ def oblicz_cene_brutto(cena_netto):
 def oblicz_cene_brutto_v2(cena_netto, stawka_vat):
     return cena_netto * (1 + stawka_vat)
 
+def oblicz_cene_brutto_v3(cena_netto, kategoria):
+    if kategoria == "usługi":
+        vat = 0.08
+    else:
+        vay = 0.23
+    return cena_netto * (1 + vat)
+
 cena = int(input("Podaj cenę netto: "))
 cena_brutto = oblicz_cene_brutto(cena)
 print(cena_brutto)
