b223a7ea17dd3b263191521a0d67be43560b769d
Patrycja <patty.zaw@gmail.com>
2021-10-21 16:32:57 +0200

    zadanie11

--- materialy-z-zajec/Zadania domowe/homework_11_pz.py
+++ materialy-z-zajec/Zadania domowe/homework_11_pz.py
@@ -2,4 +2,8 @@
 
 Stwórz tuple zawierającą informacje o osobie: imię, nazwisko, wiek, adres
 
-Użyj poszczególnych elementów krotki, żeby uzupełnić zdanie opisujące tę osobę."""
+Użyj poszczególnych elementów krotki, żeby uzupełnić zdanie opisujące tę osobę.
+
+"""
+tuple = ("Jan", "Kowalski", 44, "Mickiewicza 4")
+print(f"{tuple[0]} {tuple[1]} ma {tuple[2]} lat. Mieszka na ulicy {tuple[3]}.")
