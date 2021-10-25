04bae7e85f309464e05656c6f6c037ab759cde64
Patrycja <patty.zaw@gmail.com>
2021-10-25 20:03:03 +0200

    import

---
+++ materialy-z-zajec/8_funkcje_importy/poznawanie_modulu_datetime.py
@@ -0,0 +1,16 @@
+import datetime
+now = datetime.datetime.now()
+print(now)
+print(now.strftime("%B"))
+print(now.strftime("%B -> %A"))
+
+rok_od_teraz = datetime.timedelta(days=365)
+
+today = datetime.date.today()
+print(today)
+print(today + rok_od_teraz)
+
+swieta = datetime.date(year=2021, month=12, day=24)
+print(swieta)
+do_swiat = swieta - today
+print(do_swiat.days)
