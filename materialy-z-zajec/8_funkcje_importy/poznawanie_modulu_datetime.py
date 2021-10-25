6fb2e3b17f0a80c15e4f1a2e2b5b9862591c936c
Patrycja <patty.zaw@gmail.com>
2021-10-25 20:07:36 +0200

    import

--- materialy-z-zajec/8_funkcje_importy/poznawanie_modulu_datetime.py
+++ materialy-z-zajec/8_funkcje_importy/poznawanie_modulu_datetime.py
@@ -14,3 +14,6 @@ swieta = datetime.date(year=2021, month=12, day=24)
 print(swieta)
 do_swiat = swieta - today
 print(do_swiat.days)
+
+napis_z_data = "25-10-2021"
+print(datetime.datetime.strptime(napis_z_data, "%d-%m-%Y"))
