2c67539f9c64f30fd0f557869b11794095648e63
Patrycja <patty.zaw@gmail.com>
2021-10-25 19:50:01 +0200

    import

--- materialy-z-zajec/8_funkcje_importy/jak_dziala_import.py
+++ materialy-z-zajec/8_funkcje_importy/jak_dziala_import.py
@@ -1,2 +1,20 @@
-import random
-random.choice([1,2,3])
+# import random
+# print(random.choice([1,2,3]))
+# # dir - > rozpoznaje co ma random
+# print(dir(random))
+# print(random.randint(10,100))
+
+"""Skrót random"""
+# import random as rd
+# print(rd.choice([1,2,3]))
+# # dir - > rozpoznaje co ma random
+# print(dir(rd))
+# print(rd.randint(10,100))
+
+from random import choice, randint
+print (choice(["kot", "pies"]))
+print(randint(10,100))
+
+from sys import *
+print(dir())
+
