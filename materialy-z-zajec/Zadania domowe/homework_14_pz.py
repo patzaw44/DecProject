23ea788c12cb51bbe516ff71e2e516e6acb90002
Patrycja <patty.zaw@gmail.com>
2021-11-02 14:28:39 +0100

    homework 14

--- materialy-z-zajec/Zadania domowe/homework_14_pz.py
+++ materialy-z-zajec/Zadania domowe/homework_14_pz.py
@@ -6,3 +6,21 @@ Napisz program (używając funkcji), który przyjmuje od użytkownika napis skł
 
 np. kot-pies-albatros-czapla-delfin --> albatros-czapla-delfin-kot-pies
 """
+
+UZYTKOWNIK = input("Napisz cokolwiek: ").split(' ')
+# uzytkownik = uzytkownik.split(' ')
+# print(uzytkownik)
+
+
+def tekst_uzytkownika():
+    print('-'.join(UZYTKOWNIK))
+
+
+def posortowany_tekst_uzytkownika():
+    UZYTKOWNIK.sort()
+    print('-'.join(UZYTKOWNIK))
+
+
+tekst_uzytkownika()
+posortowany_tekst_uzytkownika()
+
