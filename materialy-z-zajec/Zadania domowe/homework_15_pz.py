0e9c74142c6d8fddcee89c964613e32ef79ab94a
Patrycja <patty.zaw@gmail.com>
2021-11-02 13:58:30 +0100

    homework 15

--- materialy-z-zajec/Zadania domowe/homework_15_pz.py
+++ materialy-z-zajec/Zadania domowe/homework_15_pz.py
@@ -4,3 +4,20 @@ Zadanie 15
 Napisz program, który zapyta użytkownika o jakieś zdanie, a następnie policzy i wypisze liczbę małych
  i dużych liter w tym zdaniu.
  """
+# uzytkownik = input("Napisz jakieś zdanie: ")
+# uzytkownik = uzytkownik.split(' ')
+# bez_spacji = ''.join(uzytkownik)
+
+uzytkownik = ''.join((input("Napisz jakieś zdanie: ")).split(' '))
+
+print(uzytkownik)
+liczba_liter = {}
+for litera in uzytkownik:
+    liczba_liter.setdefault(litera, 0)
+    liczba_liter[litera] = liczba_liter[litera]+1
+print(liczba_liter)
+
+
+
+
+
