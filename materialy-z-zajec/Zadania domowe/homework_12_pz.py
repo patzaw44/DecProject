25b2bb918b3e12b54b8644d4ddebf48107a3129f
Patrycja <patty.zaw@gmail.com>
2021-11-02 10:43:03 +0100

    homework 9

--- materialy-z-zajec/Zadania domowe/homework_12_pz.py
+++ materialy-z-zajec/Zadania domowe/homework_12_pz.py
@@ -12,3 +12,34 @@ np. literka a to 1 punkt, literka b to 3 punkty itd.
 
 -> należy uwzględnić, że jakaś literka w wyrazie podanym przez użytkownika nie będzie uwzględniona w punktacji
 (można przyjąć, że dostaje on za nią 0 punktów)"""
+
+WYRAZ = list(input("Wprowadź wyraz: ").lower())
+print(WYRAZ)
+PUNKTACJA = {'a': 1, 'b': 3, 'c': 2, 'd': 2, 'e': 1, 'f': 5, 'g': 3, 'h': 3, 'i': 1, 'j': 3, 'k': 2, 'l': 2, 'm': 2,
+             'n': 1, 'o': 1, 'p': 2, 'q': 5, 'r': 1, 's': 1, 't': 2, 'u': 3, 'w': 1, 'v': 5, 'y': 2, 'x': 3, 'z': 1}
+
+
+
+
+def obliczanie_punktow():
+
+    wyraz_pkty = {litera: PUNKTACJA.get(litera) for litera in WYRAZ}
+    # if WYRAZ != PUNKTACJA:
+    # # wyraz_pkty.pop((not PUNKTACJA.keys(), 0))
+    #     new_wyraz = {"0": 0}
+    #     WYRAZ.append(new_wyraz)
+
+    if not WYRAZ in wyraz_pkty.keys():
+            print("))))")
+
+
+
+    # if wyraz_pkty.values() == "None":
+    #     # wyraz_pkty.pop((not PUNKTACJA.keys(), 0))
+    #     new_wyraz = {"0": 0}
+    #     WYRAZ.append(new_wyraz)
+    # print(wyraz_pkty)
+    pkty = int(sum(wyraz_pkty.values()))
+    print(f"Suma wynosi: {pkty}.")
+
+obliczanie_punktow()
--- materialy-z-zajec/Zadania domowe/homework_13_pz.py
+++ materialy-z-zajec/Zadania domowe/homework_13_pz.py
@@ -7,3 +7,81 @@ Wybierz tekst dwóch wybranych przez Ciebie piosenek. Napisz program, który:
 
     słowa unikalne dla obu piosenek
 """
+song_one = """" Start spreading the news
+I am leaving today
+I want to be a part of it
+New York, New York
+These vagabond shoes
+They are longing to stray
+Right through the very heart of it
+New York, New York
+I want to wake up in a city
+That never sleeps
+And find I am king of the hill
+Top of the heap
+These small town blues
+They are melting away
+I will make a brand new start of it
+In old New York
+If I can make it there
+I will make it anywhere
+It is up to you
+New York, New York
+New York, New York
+I want to wake up in a city
+That does not sleep
+And find that I am number one
+Top of the list
+Head of the heap
+King of the hill
+These little town blues
+They have all melted away
+I am gonna make a brand new start of it
+In old New York
+And
+If I can make it there
+I will make it practically anywhere
+It is up to you
+New York, New York
+New York"""
+
+song_two = """That is life
+That is what all the people say
+You are riding high in April, shot down in May
+But I know I am gonna change that tune
+When I am back on top, back on top in June
+I said that is life
+And as funny as it may seem
+Some people get their kicks
+Stomping on a dream
+But I do not let it, let it get me down
+Cause this fine old world, it keeps spinning around
+I have been a puppet, a pauper, a pirate, a poet
+A pawn and a king
+I have been up and down and over and out
+And I know one thing
+Each time I find myself
+Flat on my face
+I pick myself up and get
+Back in the race
+That is life
+I tell you, I can not deny it
+I thought of quitting, baby
+But my heart just is not gonna buy it
+And if I did not think it was worth one single try
+I would jump right on a big bird and then I would fly
+I have been a puppet, a pauper, a pirate, a poet
+A pawn and a king
+I've been up and down and over and out
+And I know one thing
+Each time I find myself laying
+Flat on my face
+I just pick myself up and get
+Back in the race
+That is life
+That is life and I can not deny it
+Many times I thought of cutting out but my heart will not buy it
+But if there is nothing shaking come this here July
+I am gonna roll myself up
+In a big ball and die
+My, my """
