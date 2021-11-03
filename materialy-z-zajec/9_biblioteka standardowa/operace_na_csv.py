d752a77bf8bd0e6ce9f71f09750c7b5907e1bd30
Patrycja <patty.zaw@gmail.com>
2021-11-03 20:43:32 +0100

    csv

--- materialy-z-zajec/9_biblioteka standardowa/operace_na_csv.py
+++ materialy-z-zajec/9_biblioteka standardowa/operace_na_csv.py
@@ -1,7 +1,26 @@
 import csv
-with open("plik.csv", encoding="utf-8") as csvfile:
-    print(csvfile)
-    reader = csv.reader(csvfile)
-    print(reader)
-    for wiersz in reader:
-        print(wiersz)
+
+def wywolaj_zwykly_czytacz():
+    with open('plik.csv', encoding="utf-8") as csvfile:
+        print(csvfile)
+        czytacz = csv.reader(csvfile)
+        print(czytacz)
+        numer_wiersza = 0
+        for wiersz in czytacz:
+            if numer_wiersza == 0:
+                numer_wiersza += 1
+                continue
+            numer_wiersza += 1
+            imie = wiersz[0]
+            pensja = int(wiersz[4]) # wiersz[-1]
+            if pensja > 2000:
+                print(imie)
+            # print(wiersz)
+
+def wywolaj_slownikowy_czytacz():
+    with open("plik.csv", encoding="utf-8") as plik_csv:
+        czytacz = csv.DictReader(plik_csv)
+        for wiersz in czytacz:
+            print(wiersz['stanowisko'])
+
+wywolaj_slownikowy_czytacz()
