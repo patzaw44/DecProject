3cf2d9e5ee33ad243a543e0d6c8dad5baf4503b8
Patrycja <patty.zaw@gmail.com>
2021-11-12 19:34:37 +0100

    homework_26_pz

---
+++ materialy-z-zajec/Zadania domowe/homework_26_pz.py
@@ -0,0 +1,7 @@
+"""
+Zadanie 26
+
+Napisz własny generator imion i nazwisk. Przygotuj dwa pliki: jeden z imionami, drugi z nazwiskami (można więcej,
+jeżeli chcemy mieć podział na płeć). Zapytaj użytkownika ile tożsamości (imię + nazwisko) chce wygenerować,
+wygeneruj je na podstawie imion i nazwisk ze wspomnianych plików, a następnie zapisz do nowego pliku
+"""
