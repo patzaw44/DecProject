07284e336a51785698d36a49ce9a8b1da6049ba7
Patrycja <patty.zaw@gmail.com>
2021-11-12 19:33:21 +0100

    homework_25_pz

---
+++ materialy-z-zajec/Zadania domowe/homework_25_pz.py
@@ -0,0 +1,8 @@
+"""
+Zadanie 25
+
+Użyj biblioteki psutil (https://pypi.org/project/psutil/) i napisz funkcję, która sprawdzi, ile procesorów ma komputer,
+ na którym właśnie uruchamiany jest ten program.
+
+Dokumentacja psutil dostępna pod https://psutil.readthedocs.io/en/latest/
+"""
