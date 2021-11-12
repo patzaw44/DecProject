9b7ba4be9ce387b9fa82a884cea6397b7d7afc4e
Patrycja <patty.zaw@gmail.com>
2021-11-12 19:32:02 +0100

    homework_24_pz

---
+++ materialy-z-zajec/Zadania domowe/homework_24_pz.py
@@ -0,0 +1,29 @@
+"""
+Zadanie 24
+
+Tym razem trochę inne zadania - jego rozwiązanie to wrzucenie do repozytorium odpowiednich screenów
+
+1) Stwórz dwa wirtualne środowiska, nazwij je (twoje imię)_projektA, (twoje imię)_projektB - np. dla mnie miałyby
+one nazwy mateusz_projektA, mateusz_projektB 2) Dla środowiska dla projektu A, zainstaluj:
+
+    faker -> biblioteka do generowania losowych danych (https://pypi.org/project/Faker/)
+
+    python-dateutil -> biblioteka zawierająca trochę ułatwiaczy dla datetime (https://pypi.org/project/python-dateutil/)
+
+3) Dla środowiska dla projektu B, zainstaluj:
+
+    django - aplikacje webowe
+
+    pandas - analiza danych
+
+    colorama - możliwość kolorowania tekstu w terminalu (może się przydać na kolejne hackatony ;) )
+
+4) Załącz w repozytorium screeny z bibliotekami zainstalowanymi dla każdego ze środowisk (niech nazwy plików mają w
+nazwie zadanie_domowe_24 :)
+
+Stworzenie virtualenvów i instalowanie paczek można zrobić przez wiersz poleceń lub pycharma.
+W przypadku tego pierwszego interesuje mnie screen z użytymi komendami oraz wynik komendy pip freeze.
+W przypadku korzystania z narzędzi oferowanych przez Pycharma wystarczy screen z listą zależności zainstalowanych
+dla konkretnego środowiska (settings -> project-> Python Interpreter i tam wybieramy z listy rozwijanej).
+W razie pytań, służę pomocą.
+"""
