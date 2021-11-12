03ca0e922cf3688574fe098efedde5bca35d8d4e
Patrycja <patty.zaw@gmail.com>
2021-11-12 19:38:08 +0100

    additional task

---
+++ materialy-z-zajec/Zadania domowe/additional_task_3_pz.py
@@ -0,0 +1,6 @@
+"""
+Zadanie dodatkowe 3
+
+Napisz w Pythonie konsolową wersję gry "Papier, kamień, nożyce",
+w której można zagrać przeciwko komputerowi lub z drugim graczem
+"""
---
+++ materialy-z-zajec/Zadania domowe/additional_task_4_pz.py
@@ -0,0 +1,9 @@
+"""
+Zadanie dodatkowe 4
+
+Napisz funkcję sprawdzającą, o której zadzwoni alarm. Funkcja przyjmuje dwa parametry: dzień tygodnia oraz
+zmienną logiczną, która wskazuje, czy jesteśmy na wakacjach, a powinna zwrócić napis z godziną alarmu.
+
+Reguły: dni są oznaczone liczbami od 1 do 7 (pon-nd). W dni powszednie, alarm powinien zadzwonić o 7,
+w weekendy o 10. Chyba, że jesteśmy na wakacjach, to wtedy w dni powszednie o 10, a w weekendy nie powinien dzwonić
+"""
