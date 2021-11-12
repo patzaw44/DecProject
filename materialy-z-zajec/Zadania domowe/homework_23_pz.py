bae9f23be400b3ae0694be50e73887fd3c2225ba
Patrycja <patty.zaw@gmail.com>
2021-11-12 19:31:50 +0100

    homework_23_pz

---
+++ materialy-z-zajec/Zadania domowe/homework_23_pz.py
@@ -0,0 +1,8 @@
+"""
+Zadanie 23
+
+Napisz program, który będzie zawierał funkcję, która dla tekstu przekazanego jako parametr zwróci listę wszystkich dat,
+ które wystąpiły w tym tekście. Dla uproszczenia można uznać, że interesują nas tylko daty w formacie YYYY-MM-DD.
+ Do znalezienia dat użyj modułu z wyrażeniami regularnymi re
+
+"""
