0a1af185093c770b68e75dfc1e45888f84a3e97b
Patrycja <patty.zaw@gmail.com>
2021-11-18 17:35:46 +0100

    zadanie

---
+++ materialy-z-zajec/13_wyjątki_obiektowosc/wiele_blokow_except.py
@@ -0,0 +1,8 @@
+liczby= input("Podaj 3 liczby rozdzielone przecinkiem:")
+
+lista_liczb = liczby.split(",")
+
+try:
+    print(f"Trzecia liczba to {lista_liczb[2]}")
+except IndexError:
+    print("Podałeś za mało liczb!")
