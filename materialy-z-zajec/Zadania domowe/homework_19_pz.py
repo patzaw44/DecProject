2b2bd92cfc6f3b166cd176d3c704d69493e3507c
Patrycja <patty.zaw@gmail.com>
2021-11-21 20:21:29 +0100

    homework_19_pz

--- materialy-z-zajec/Zadania domowe/homework_19_pz.py
+++ materialy-z-zajec/Zadania domowe/homework_19_pz.py
@@ -4,3 +4,14 @@ przyjmuje od użytkownika 3 liczby: dolny zakres, górny zakres oraz ilość lic
 następnie losuje zadaną ilość liczb z podanego przedziału
 
 Przykład: Użytkownik podaje liczby 1, 20, 5 -> program ma wylosować 5 liczb z zakresu 1-20 (włącznie)"""
+import random
+
+
+def losowanie(dolny_zakres, gorny_zakres, ilosc):
+    for i in range(ilosc):
+        print(random.randint(dolny_zakres, gorny_zakres))
+
+
+losowanie(dolny_zakres=int(input(f"Podaj liczbę min: ")),
+          gorny_zakres=int(input(f"Podaj liczbę max: ")),
+          ilosc=int(input(f"Podaj liczbę losowanych liczb: ")))
