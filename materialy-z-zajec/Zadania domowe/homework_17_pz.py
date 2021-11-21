fbc465547472bad95209a78e39106580657dabdc
Patrycja <patty.zaw@gmail.com>
2021-11-21 15:08:18 +0100

    homework_17_pz

--- materialy-z-zajec/Zadania domowe/homework_17_pz.py
+++ materialy-z-zajec/Zadania domowe/homework_17_pz.py
@@ -11,3 +11,19 @@ Funkcja powinna przyjąć 3 argumenty:
 3) nowy znak
 
 np. my_replace("1234", "1", "9") -> "9234" """
+
+
+def zamiana_znaku(napis, stary_znak, nowy_znak):
+    """Funkcja zamieniająca stary znak na nowy."""
+    napis = list(napis)
+    print(type(napis))
+    try:
+        znak_indeks = napis.index(stary_znak)
+        napis[znak_indeks] = nowy_znak
+        napis = ''.join(napis)
+        print(f"Po zamianie: {napis}")
+    except ValueError:
+        print("Brak podanego znaku.")
+
+
+zamiana_znaku(napis="1234", stary_znak="1", nowy_znak="9")
