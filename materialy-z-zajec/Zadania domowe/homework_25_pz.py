a769258bd3080349dd5b5f3f63df685e8f509966
Patrycja <patty.zaw@gmail.com>
2021-11-21 21:56:04 +0100

    homework_25_pz

--- materialy-z-zajec/Zadania domowe/homework_25_pz.py
+++ materialy-z-zajec/Zadania domowe/homework_25_pz.py
@@ -6,3 +6,15 @@ Użyj biblioteki psutil (https://pypi.org/project/psutil/) i napisz funkcję, kt
 
 Dokumentacja psutil dostępna pod https://psutil.readthedocs.io/en/latest/
 """
+import psutil
+
+
+def sprawdz_cpu():
+    print(f"Zwraca czasy procesora systemowego - krotka: \n{psutil.cpu_times()}\n")
+    print(f"Zwraca wykorzystanie procesora w całym systemie jako procent : \n{psutil.cpu_percent()}\n")
+    print(f"Zwraca liczbę logicznych procesorów w systemie: \n{psutil.cpu_count()}\n")
+    print(f"Zwraca różne statystyki procesora jako krotkę: \n{psutil.cpu_stats()}\n")
+    print(f"Zwraca częstotliwość procesora: \n{psutil.cpu_freq()}")
+
+
+sprawdz_cpu()
