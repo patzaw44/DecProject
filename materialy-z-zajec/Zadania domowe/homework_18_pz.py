a02c0066bf1a00574a4b1057b845789625a20823
Patrycja <patty.zaw@gmail.com>
2021-11-24 20:11:32 +0100

    homework_22

--- materialy-z-zajec/Zadania domowe/homework_18_pz.py
+++ materialy-z-zajec/Zadania domowe/homework_18_pz.py
@@ -20,33 +20,46 @@ def obl_salda_lista(transakcje):
 obl_salda_lista(transakcje=[1000, -400, 200, 1500, -500])
 
 
+# def obl_salda_krotka():
+#     transakcje = [('D', 1000), ('W', 400), ('D', 200), ('D', 1500), ('W', 500)]
+#     wyplata = []
+#     wplata = []
+#     transakcje_lista = list(sum(transakcje, ()))
+#
+#     # print(transakcje_lista)
+#
+#     for t in str(transakcje_lista):
+#         # Stworzenie dwoch list, tylko z liczbami: wplaty i wyplaty
+#         if "W" in t:
+#             wyplata=[(transakcje_lista[transakcje_lista.index(t)+1])]
+#             wyplata.append(t)
+#
+#         if "D" == t:
+#             wplata = [(transakcje_lista[transakcje_lista.index(t)+1])]
+#             wplata.append(t)
+#
+#     print(wyplata)
+#     print(wplata)
 def obl_salda_krotka():
     transakcje = [('D', 1000), ('W', 400), ('D', 200), ('D', 1500), ('W', 500)]
     wyplata = []
     wplata = []
-    transakcje_lista = list(sum(transakcje, ()))
 
-    # print(transakcje_lista)
+    for transakcja in transakcje:
+        typ_transakcji = transakcja[0]
+        kwota = transakcja[1]
+        if typ_transakcji == "W":
+            wyplata.append(kwota)
+        elif typ_transakcji == "D":
+            wplata.append(kwota)
 
-    for t in str(transakcje_lista):
-        # Stworzenie dwoch list, tylko z liczbami: wplaty i wyplaty
-        if "W" in t:
-            wyplata=[(transakcje_lista[transakcje_lista.index(t)+1])]
-            wyplata.append(t)
+    print(f"Wypłaty: {wyplata}")
+    print(f"Wpłaty: {wplata}")
 
-        if "D" == t:
-            wplata = [(transakcje_lista[transakcje_lista.index(t)+1])]
-            wplata.append(t)
-
-    print(wyplata)
-    print(wplata)
-
-    # aktualne_saldo = sum(wplata)-sum(wyplata)
-    # print(aktualne_saldo)
+    aktualne_saldo = sum(wplata)-sum(wyplata)
+    print(f"Twoje aktualne saldo: {aktualne_saldo} zł.")
 
 
 obl_salda_krotka()
 
-
-# 2 pomysl :z krotki-> slownik, w slowniku powstają dwie listy D i W.... saldo = D-W
 
