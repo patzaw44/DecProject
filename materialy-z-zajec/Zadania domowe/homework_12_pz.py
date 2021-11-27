718bb286b31715a37b5f90345dd4047a1c54c9a3
Patrycja <patty.zaw@gmail.com>
2021-11-27 19:05:05 +0100

    homework_12_pz

--- materialy-z-zajec/Zadania domowe/homework_12_pz.py
+++ materialy-z-zajec/Zadania domowe/homework_12_pz.py
@@ -17,6 +17,7 @@ np. literka a to 1 punkt, literka b to 3 punkty itd.
 def obliczanie_punktow(wyraz):
     punktacja = {'a': 1, 'b': 3, 'c': 2, 'd': 2, 'e': 1, 'f': 5, 'g': 3, 'h': 3, 'i': 1, 'j': 3, 'k': 2, 'l': 2, 'm': 2,
                  'n': 1, 'o': 1, 'p': 2, 'q': 5, 'r': 1, 's': 1, 't': 2, 'u': 3, 'w': 1, 'v': 5, 'y': 2, 'x': 3, 'z': 1}
+
     suma = 0
     for litera in wyraz:
         if litera not in punktacja:
