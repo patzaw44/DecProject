d17b017a7dd845642cd0a35f5f361d7fe1ed7e92
Patrycja <patty.zaw@gmail.com>
2021-12-20 14:11:32 +0100

    zad

--- SimpleApp/app/django_app/admin.py
+++ SimpleApp/app/django_app/admin.py
@@ -1,17 +1,17 @@
 from django.contrib import admin
-from .models import CarBrand, CarModel, CarsId
-"""Add_rate, Average_rate, Car_deteles, Number_of_rates, Cars_data, Rate, CarsDetail"""
+from .models import CarBrand, CarModel, CarId, AddRate, AverageRate, CarsData, CarDetele, NumberOfRate, Rate, CarDetail
+
 
 # Register your models here.
 
 admin.site.register(CarBrand)
 admin.site.register(CarModel)
-admin.site.register(CarsId)
-# admin.site.register(Add_rate)
-# admin.site.register(Average_rate)
-# admin.site.register(Car_deteles)
-# admin.site.register(Number_of_rates)
-#
-# admin.site.register(Cars_data)
-# admin.site.register(Rate)
-# admin.site.register(Cars_detail)
+admin.site.register(CarId)
+admin.site.register(AddRate)
+admin.site.register(AverageRate)
+admin.site.register(CarDetele)
+admin.site.register(NumberOfRate)
+
+admin.site.register(CarsData)
+admin.site.register(Rate)
+admin.site.register(CarDetail)
--- SimpleApp/app/django_app/models.py
+++ SimpleApp/app/django_app/models.py
@@ -2,54 +2,55 @@ from django.db import models
 
 
 class CarBrand(models.Model):
-    car_make2 = models.CharField(max_length=40)
+    car_make = models.CharField(max_length=40)
 
 
 class CarModel(models.Model):
-    model_name2 = models.CharField(max_length=40)
+    model_name = models.CharField(max_length=40)
 
 
-class CarsId(models.Model):
-    car_id2 = models.CharField(max_length=40)
+class CarId(models.Model):
+    car_id = models.IntegerField()
 
 
-# class AddRate(models.Model):
-#     rating2 = models.IntegerField()
-#
-#
-# class AverageRate(models.Model):
-#     avg_rating2 = models.FloatField()
-#
-#
-# # Usuwanie po id
-# class CarDeteles(models.Model):
-#     id = models.ForeignKey('Car_ids', on_delete=models.CASCADE)
-#
-#
-# class NumberOfRates(models.Model):
-#     rates_rating = models.IntegerField()
-#
-#
-# class CarsData(models.Model):
-#     car_make = models.ForeignKey('Car_brand', on_delete=models.CASCADE)
-#     model_name = models.ForeignKey('Car_model', on_delete=models.CASCADE)
-#
-#     def __str__(self):
-#         return self.car_make
-#
-#
-# class Rate(models.Model):
-#     """Add a rate for a car from 1 to 5 """
-#     id = models.ForeignKey('Car_ids', on_delete=models.CASCADE)
-#     rating = models.ForeignKey('Add_rate', on_delete=models.CASCADE)
-#
-#
-# class CarsDetail(models.Model):
-#     id = 'car_id'
-#     make = models.ForeignKey('Car_brand', on_delete=models.CASCADE)
-#     model = models.ForeignKey('Car_model', on_delete=models.CASCADE)
-#     avg_rating = models.ForeignKey('Average_rate', on_delete=models.CASCADE)
-#     rates_rating = models.ForeignKey('Number_of_rates', on_delete=models.CASCADE)
-#
-#     def __str__(self):
-#         return self.make
+class AddRate(models.Model):
+    rating = models.IntegerField()
+
+
+class AverageRate(models.Model):
+    avg_rating = models.FloatField()
+
+
+# Usuwanie po id
+class CarDetele(models.Model):
+    Primary_key = True
+    new_id = models.ForeignKey('CarId', on_delete=models.CASCADE)
+
+
+class NumberOfRate(models.Model):
+    rates_rating = models.IntegerField()
+
+
+class CarsData(models.Model):
+    car_make = models.ForeignKey('CarBrand', on_delete=models.CASCADE)
+    model_name = models.ForeignKey('CarModel', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.car_make
+
+
+class Rate(models.Model):
+    """Add a rate for a car from 1 to 5 """
+    # new_id = models.ForeignKey('CarId', on_delete=models.CASCADE)
+    rating = models.ForeignKey('AddRate', on_delete=models.CASCADE)
+
+
+class CarDetail(models.Model):
+    # new_id2 = models.ForeignKey('CarId', on_delete=models.CASCADE)
+    make = models.ForeignKey('CarBrand', on_delete=models.CASCADE)
+    model = models.ForeignKey('CarModel', on_delete=models.CASCADE)
+    avg_rating = models.ForeignKey('AverageRate', on_delete=models.CASCADE)
+    rates_rating = models.ForeignKey('NumberOfRate', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.make
