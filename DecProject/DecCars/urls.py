aa1af0bf8891d1553879853cf389832b5ce5abd2
Patrycja <patty.zaw@gmail.com>
2021-12-29 20:14:46 +0100

    third

--- DecProject/DecCars/urls.py
+++ DecProject/DecCars/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from django.urls import include, path
-from DecApp import urls
+from django.urls import path
+from DecCars.decapp.views import test_response
+
+from . import decapp
+from .decapp import urls
+from django.conf.urls import include
 from rest_framework import routers
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(urls))
+    path('test/', test_response)
 ]
