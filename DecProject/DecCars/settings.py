dbba98d6ead0379460c00c5b95411fe4dc997df1
Patrycja <patty.zaw@gmail.com>
2021-12-30 07:29:35 +0100

    do heroku

--- DecProject/DecCars/settings.py
+++ DecProject/DecCars/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['december-cars-app.herokuapp.com', 'localhost:8000']
 
 
 # Application definition
